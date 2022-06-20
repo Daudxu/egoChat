7122b3054d39c382d5797f77f6067a60cb104b56
xudan <xudan@apowo.com>
2022-06-20 18:06:34 +0800

    fix update

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,11 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import VueSocketIO from "vue-3-socket.io";
 
-createApp(App).use(store).use(router).mount("#app");
+const socketObj = new VueSocketIO({
+  debug: true,
+  connection: "http://metinseylan.com:1992",
+});
+
+createApp(App).use(socketObj).use(store).use(router).mount("#app");
